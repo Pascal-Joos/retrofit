549d66fbd65533b571e2a192458ba4fcc6b32f8e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 14:34:47 +0000

    Revert "fix: 7 - DEREFERENCE_NULLABLE - multipartBuilder.addPart(part); - dereferenced expression multipartBuilder is @Nullable"
    This reverts commit 9ee01cad7987bdf2cac1759ad1faf18d15171024.

--- retrofit/src/main/java/retrofit2/RequestBuilder.java
+++ retrofit/src/main/java/retrofit2/RequestBuilder.java
@@ -221,9 +221,6 @@ final class RequestBuilder {
 
   @SuppressWarnings("ConstantConditions") // Only called when isMultipart was true.
   void addPart(MultipartBody.Part part) {
-    if (multipartBuilder == null) {
-      throw new IllegalStateException("Multipart builder not initialized");
-    }
     multipartBuilder.addPart(part);
   }
 
