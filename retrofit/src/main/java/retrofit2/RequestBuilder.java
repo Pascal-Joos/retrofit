3d11dd37a2429a504a9bd671b7ca202a645066df
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 09:30:07 +0000

    fix: 8 - DEREFERENCE_NULLABLE - urlBuilder.addEncodedQueryParameter(name, value); - dereferenced expression urlBuilder is @Nullable

--- retrofit/src/main/java/retrofit2/RequestBuilder.java
+++ retrofit/src/main/java/retrofit2/RequestBuilder.java
@@ -53,7 +53,7 @@ final class RequestBuilder {
 
   private final HttpUrl baseUrl;
   private @Nullable String relativeUrl;
-  private @Nullable HttpUrl.Builder urlBuilder;
+  @Nullable private HttpUrl.Builder urlBuilder;
 
   private final Request.Builder requestBuilder;
   private final Headers.Builder headersBuilder;
@@ -187,13 +187,17 @@ final class RequestBuilder {
 
   void addQueryParam(@Nullable String name, @Nullable String value, boolean encoded) {
     if (relativeUrl != null) {
-      // Do a one-time combination of the built relative URL and the base URL.
       urlBuilder = baseUrl.newBuilder(relativeUrl);
       if (urlBuilder == null) {
         throw new IllegalArgumentException(
             "Malformed URL. Base: " + baseUrl + ", Relative: " + relativeUrl);
       }
       relativeUrl = null;
+    }
+
+    HttpUrl.Builder urlBuilder = this.urlBuilder;
+    if (urlBuilder == null) {
+      throw new IllegalStateException("urlBuilder == null");
     }
 
     if (encoded) {
