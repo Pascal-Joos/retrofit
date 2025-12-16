087fe7e37c12c36e3a5565e58e282de10bfe44f8
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:43:52 +0000

    fix: 4 - DEREFERENCE_NULLABLE - formBuilder.addEncoded(name, value); - dereferenced expression formBuilder is @Nullable

--- retrofit/src/main/java/retrofit2/RequestBuilder.java
+++ retrofit/src/main/java/retrofit2/RequestBuilder.java
@@ -207,6 +207,9 @@ final class RequestBuilder {
 
   @SuppressWarnings("ConstantConditions") // Only called when isFormEncoded was true.
   void addFormField(String name, String value, boolean encoded) {
+    if (formBuilder == null) {
+      throw new IllegalStateException("Form body type was not set to form-encoded.");
+    }
     if (encoded) {
       formBuilder.addEncoded(name, value);
     } else {
