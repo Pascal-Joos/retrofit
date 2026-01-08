cca0eeb2c519e61efe4353f0c7b7c30c0868e6e1
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 09:54:35 +0000

    fix: 1 - DEREFERENCE_NULLABLE - if (requestFactory.httpMethod.equals("HEAD") && !Void.class.equals(responseType)) { - dereferenced expression requestFactory.httpMethod is @Nullable

--- retrofit/src/main/java/retrofit2/HttpServiceMethod.java
+++ retrofit/src/main/java/retrofit2/HttpServiceMethod.java
@@ -18,6 +18,7 @@ package retrofit2;
 import static retrofit2.Utils.getRawType;
 import static retrofit2.Utils.methodError;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
@@ -77,7 +78,8 @@ abstract class HttpServiceMethod<ResponseT, ReturnT> extends ServiceMethod<Retur
       throw methodError(method, "Response must include generic type (e.g., Response<String>)");
     }
     // TODO support Unit for Kotlin?
-    if (requestFactory.httpMethod.equals("HEAD") && !Void.class.equals(responseType)) {
+    if (Nullability.castToNonnull(requestFactory.httpMethod).equals("HEAD")
+        && !Void.class.equals(responseType)) {
       throw methodError(method, "HEAD method must use Void as response type.");
     }
 
