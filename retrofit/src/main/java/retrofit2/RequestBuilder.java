90baaa785d39b279b0c723cf43d7f823305dea08
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 08:06:17 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression multipartBuilder is @Nullable' /home/vscode/nullness-benchmarks/retrofit/retrofit/src/main/java/retrofit2/RequestBuilder.java:219     multipartBuilder.addPart(headers, body);

--- retrofit/src/main/java/retrofit2/RequestBuilder.java
+++ retrofit/src/main/java/retrofit2/RequestBuilder.java
@@ -60,7 +60,7 @@ final class RequestBuilder {
   private @Nullable MediaType contentType;
 
   private final boolean hasBody;
-  private @Nullable MultipartBody.Builder multipartBuilder;
+  @Nullable private MultipartBody.Builder multipartBuilder;
   @Nullable private FormBody.Builder formBuilder;
   private @Nullable RequestBody body;
 
@@ -220,11 +220,19 @@ final class RequestBuilder {
 
   @SuppressWarnings("ConstantConditions") // Only called when isMultipart was true.
   void addPart(Headers headers, @Nullable RequestBody body) {
+    if (multipartBuilder == null) {
+      multipartBuilder = new MultipartBody.Builder();
+      multipartBuilder.setType(MultipartBody.FORM);
+    }
     multipartBuilder.addPart(headers, body);
   }
 
   @SuppressWarnings("ConstantConditions") // Only called when isMultipart was true.
   void addPart(MultipartBody.Part part) {
+    if (multipartBuilder == null) {
+      multipartBuilder = new MultipartBody.Builder();
+      multipartBuilder.setType(MultipartBody.FORM);
+    }
     multipartBuilder.addPart(part);
   }
 
